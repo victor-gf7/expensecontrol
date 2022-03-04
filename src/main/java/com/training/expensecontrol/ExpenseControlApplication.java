05224c5e2e757a4446cb87ecf7dc4465c424032d
victor-gf7 <ferreirazaltan@gmail.com>
2022-03-04 17:07:07 -0300

    feat: Initial Project

---
+++ src/main/java/com/training/expensecontrol/ExpenseControlApplication.java
@@ -0,0 +1,13 @@
+package com.training.expensecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/expensecontrol/ExpenseControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.expensecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
